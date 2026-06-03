35c4115397b1b5495bb30d03da17091a4025f457
Mariia <lebedeva@satel.org>
2026-06-03 17:41:58 +0300

    refactor: update comments

--- lib/src/exceptions.dart
+++ lib/src/exceptions.dart
@@ -1,6 +1,6 @@
+// ignore_for_file: public_member_api_docs
+
 /// Base exception class for all OHTTP library errors.
-///
-/// This provides a common type for catching any OHTTP-related exception
 // ignore: prefer-match-file-name
 abstract class OhttpException implements Exception {
   final String message;
